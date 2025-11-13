dcbb903ce32b9af0c0cffb0a67cd681b4c23375d
Miguel Diogo <miguel.g.diogo@gmail.com>
2025-11-13 18:47:00 +0000

    ⚡️ remove unnecessary pages and centralize account verification in a single page

--- src/php/app/controllers/VerificationController.php
+++ src/php/app/controllers/VerificationController.php
@@ -12,19 +12,23 @@ class VerificationController extends Controller {
 		$this->verificationModel = new VerificationModel;
 	}
 
-	public function index() {
+	public function verify() {
 		$token = $_GET['token'];
 
 		$user = $this->userModel->getUserByToken($token);
 
 		if (!$token || !$user) {
-			$this->render('/pages/invalid_verification', ['title' => 'Camagru']);
+			$this->render('/pages/verification', ['title' => 'Camagru', 'image' => '/images/404.png', 'result_title' => 'Invalid or expired link', 'message' => 'Invalid account verification request.']);
 			return;
 		} else if ($user->verified) {
-			$this->render('/pages/user_already_verified', ['title' => 'Camagru']);
+			$this->render('/pages/verification', ['title' => 'Camagru', 'image' => '/images/404.png', 'result_title' => 'User already verified', 'message' => 'You should already be able to login using your credentials.']);
 			return;
 		}
 		$this->verificationModel->setVerified($user->id);
-		$this->render('/pages/successful_verification', ['title' => 'Camagru']);
+		$this->render('/pages/verification', ['title' => 'Camagru', 'image' => '/images/success.jpg', 'result_title' => 'Verification successful', 'message' => 'You should now be able to login using your credentials.']);
+	}
+
+	public function change_details() {
+		
 	}
 }
--- src/php/app/views/pages/verification.php
+++ src/php/app/views/pages/verification.php
@@ -1,5 +1,5 @@
 <?php
-	include_once '../app/views/templates/header.php';
+	include_once '../app/views/templates/head.php';
 ?>
 
 <body class="md:bg-[url(/images/bg_home.jpg)] bg-cover bg-center bg-no-repeat min-h-screen">
@@ -7,11 +7,11 @@
 		<div class="flex flex-wrap md:flex-nowrap bg-white w-full max-w-6xl h-screen md:h-auto md:max-h-[768px] justify-center items-center md:rounded-xl
 			md:shadow-[0px_5px_15px_rgba(0,0,0,0.35)]">
 			<div class="flex flex-col md:flex-row justify-center items-center w-full md:min-h-screen max-w-sm md:max-w-3xl">
-				<img class="w-1/2 md:w-full max-w-sm" src="/images/404.png">
+				<img class="w-1/2 md:w-full max-w-sm" src="<?= htmlspecialchars($image)?>">
 				<div class="flex flex-col w-full">
-					<span class="text-center md:text-left font-[Montserrat] font-bold text-3xl">User already verified</span>
-					<span class="pt-4 text-center md:text-left font-[Montserrat] text-xl">You should already be able to login using your credentials.</span>
-					<a href="/login" class="pt-4 text-center md:text-left font-[Montserrat] text-xl">Go to <span class="cursor-pointer hover:underline text-blue-600 font-semibold">homepage.</span></a>
+					<span class="text-center md:text-left font-[Montserrat] font-bold text-3xl"><?= htmlspecialchars($result_title)?></span>
+					<span class="pt-4 text-center md:text-left font-[Montserrat] text-xl"><?= htmlspecialchars($message)?></span>
+					<a href="/" class="pt-4 text-center md:text-left font-[Montserrat] text-xl">Go to <span class="cursor-pointer hover:underline text-blue-600 font-semibold">homepage.</span></a>
 				</div>
 			</div>
 		</div>
